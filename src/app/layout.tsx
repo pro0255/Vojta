970ab0eb22859239fb7c24595ce6aa7a771d5c22
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-07-13 21:07:11 +0200

    Added custom futura font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import { FC, ReactNode } from 'react'
 import { Metadata } from 'next'
 import Head from 'next/head'
 import { PageContainer } from '@/components/DesignSystem/Layout/PageContainer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export type LayoutProps = { children: ReactNode }
 
@@ -19,7 +16,7 @@ const RootLayout: FC<LayoutProps> = ({ children }) => {
       <Head>
         <link rel="icon" href="/favicon/favicon-16x16.png" />
       </Head>
-      <body className={inter.className}>
+      <body>
         <PageContainer>{children}</PageContainer>
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +8,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['futura'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
