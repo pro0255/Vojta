5104d7d2f3fdd9533badb31e0820bcbca36852a3
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-08-09 22:56:52 +0200

    Added altas internship

---
+++ src/work/atlas_internship.ts
@@ -0,0 +1,11 @@
+import { WorkName, WorkTag, WorkType } from '@/work/types'
+
+export const atlasInternship: WorkType = {
+  title: 'ATLAS GROUP - internship',
+  description: 'this is description',
+  name: WorkName.company,
+  work: [],
+  tags: [WorkTag.FE, WorkTag.MOBX, WorkTag.React],
+  thumbnail: 'atlas.jpeg',
+  link: 'https://www.seznam.cz/',
+}
--- src/work/index.ts
+++ src/work/index.ts
@@ -3,5 +3,13 @@ import { masterDegree } from '@/work/master'
 import { ataccama } from '@/work/ataccama'
 import { czechitas } from '@/work/czechitas'
 import { bachelorDegree } from '@/work/bachelor'
+import { atlasInternship } from '@/work/atlas_internship'
 
-export const work = [bachelorDegree, atlas, masterDegree, ataccama, czechitas]
+export const work = [
+  bachelorDegree,
+  atlasInternship,
+  atlas,
+  masterDegree,
+  ataccama,
+  czechitas,
+]
