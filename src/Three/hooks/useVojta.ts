13799b89fbc5eeec613a41f47c939bfc2e15dfc6
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-08-28 20:46:01 +0200

    feat: added useVojtaAnimations

--- src/Three/hooks/useVojta.ts
+++ src/Three/hooks/useVojta.ts
@@ -1,16 +1,11 @@
-import { useAnimation } from '@/Three/hooks/useAnimation'
 import { useAnimations } from '@react-three/drei'
 import { useEffect, useRef } from 'react'
 import { VojtaState } from '@/Three/store/types'
+import { useVojtaAnimation } from '@/Three/hooks/useVojtaAnimation'
 
 export const useVojta = () => {
   const groupRef = useRef(null)
-
-  const animation = useAnimation({
-    path: 'animations/vojta/young_adult',
-    animation: VojtaState.Talking,
-  })
-
+  const animation = useVojtaAnimation({ animation: VojtaState.Talking })
   const { actions } = useAnimations([animation], groupRef)
 
   useEffect(() => {
