d6f01d29e1d93e4e7c2261b278d63d46e15fab0d
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-10-01 12:48:44 +0200

    feat: flashmessages container

--- src/components/FlashMessage/FlashMessageContainer.tsx
+++ src/components/FlashMessage/FlashMessageContainer.tsx
@@ -2,6 +2,7 @@
 
 import { useFlashMessages } from '@/components/FlashMessage/flashMessagesStore'
 import { FlashMessageView } from '@/components/FlashMessage/FlashMessageView'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const FlashMessageContainer = () => {
   const flashMessages = useFlashMessages(
@@ -9,13 +10,23 @@ export const FlashMessageContainer = () => {
   )
 
   return (
-    <div>
+    <div className={'fixed bottom-[5%] left-[5%] z-50'}>
       <ul>
         {flashMessages.map(flashMessage => {
           return (
-            <li key={flashMessage.id}>
-              <FlashMessageView {...flashMessage} />
-            </li>
+            <AnimatePresence key={flashMessage.id}>
+              <motion.li
+                className={'mb-2'}
+                initial={{ opacity: 0, transform: 'translateY(50%)' }}
+                animate={{ opacity: 1, transform: 'translateY(0%)' }}
+                exit={{ opacity: 0, transform: 'translateY(-50%)' }}
+                transition={{
+                  duration: 0.6,
+                }}
+              >
+                <FlashMessageView {...flashMessage} />
+              </motion.li>
+            </AnimatePresence>
           )
         })}
       </ul>
--- src/components/FlashMessage/FlashMessageView.tsx
+++ src/components/FlashMessage/FlashMessageView.tsx
@@ -1,7 +1,33 @@
 import { FlashMessageType } from '@/components/FlashMessage/flashMessagesStore'
+import { GrayText } from '@/components/DesignSystem'
+import { BoldText } from '@/components/DesignSystem/Text/BoldText'
 
 type Props = FlashMessageType
 
 export const FlashMessageView = ({ id, title, content }: Props) => {
-  return <div>{content}</div>
+  return (
+    <div
+      className={
+        'flex flex-row bg-white shadow-2xl rounded w-[400px] max-h-[150px] overflow-hidden'
+      }
+    >
+      <div className={'p-4 flex flex-col w-[75%]'}>
+        <header className={'flex flex-row mb-3'}>
+          <span>
+            <BoldText>{title}</BoldText>
+          </span>
+        </header>
+        <main className={'truncate'}>
+          <GrayText>{content}</GrayText>
+        </main>
+      </div>
+      <button
+        className={
+          'transition-all duration-300 ease-in-out w-[25%] border-l-2 border-slate-100 hover:bg-slate-100 focus:bg-slate-100'
+        }
+      >
+        <GrayText>Close</GrayText>
+      </button>
+    </div>
+  )
 }
