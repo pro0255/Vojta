e8f42c6cdef5ee7fd875eb13fd284799b90e0738
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-10-08 23:02:05 +0200

    feat: adding avatars to chat

--- src/components/Chat/components/Avatars.tsx
+++ src/components/Chat/components/Avatars.tsx
@@ -10,18 +10,18 @@ export const Avatars = () => {
         fov: 30,
       }}
       style={{
-        zIndex: 10,
+        zIndex: -10,
         position: 'fixed',
         top: '0',
         left: '0',
       }}
     >
       <group>
-        <GuessMan scale={[0.7, 0.7, 0.7]} position={[-1.5, -0.5, 0]} />
+        <GuessMan scale={[0.7, 0.7, 0.7]} position={[-1.8, -1, 0]} />
       </group>
       <ambientLight intensity={1} />
       <group position-y={-1}>
-        <VojtaYoungAdult scale={[0.7, 0.7, 0.7]} position={[1.5, 0.5, 0]} />
+        <VojtaYoungAdult scale={[0.7, 0.7, 0.7]} position={[1.8, 0, 0]} />
       </group>
     </Canvas>
   )
