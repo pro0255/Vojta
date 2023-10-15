42feae9775936ecfa946e821654c677980e0f8d4
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-10-15 12:51:26 +0200

    refactor: giving czechitas a learn tag

--- src/app/work/instances/czechitas.ts
+++ src/app/work/instances/czechitas.ts
@@ -4,7 +4,7 @@ export const czechitas: WorkType = {
   title: 'Czechitas',
   description:
     'A product aimed at easing the challenging journey into the IT domain with a guiding hand. Focused on helping individuals gain their initial experiences in conveying technical information, working with people, and motivating them to overcome obstacles',
-  name: WorkName.Company,
+  name: WorkName.Learn,
   work: [],
   tags: [WorkTag.Python, WorkTag.DataScience, WorkTag.Learning],
   thumbnail: 'czechitas.jpg',
