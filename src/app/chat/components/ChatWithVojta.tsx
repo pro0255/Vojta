92a491e037c5d27c4ec9f30352fa02273a4ef60f
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-10-15 23:50:49 +0200

    fix: whatever

--- src/app/chat/components/ChatWithVojta.tsx
+++ src/app/chat/components/ChatWithVojta.tsx
@@ -6,7 +6,7 @@ import { Avatars } from '@/app/chat/components/Avatars'
 
 export const ChatWithVojta = () => {
   return (
-    <main className="flex flex-col z-20 w-[55%]">
+    <main className="flex flex-col z-20 w-[70%] md:w-[55%]">
       <Messages />
       <div className={'hidden md:inline'}>
         <Avatars />
